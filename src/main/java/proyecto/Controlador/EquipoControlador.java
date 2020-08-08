38014a9eda75aae6d0792a2f0e2bdd01dfb38b34
Pedro Donoso Aguilera <pedro.donosoa@sansano.usm.cl>
2020-08-08 00:39:06 -0400

    Chopinhauer API Bienvenida Update 3

--- src/main/java/proyecto/Controlador/EquipoControlador.java
+++ src/main/java/proyecto/Controlador/EquipoControlador.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import proyecto.Entidad.Equipo;
 import proyecto.Entidad.PServicio;
