c73255dcd2d5704d7f74c594ea8a4a3affd225a9
Pedro Donoso Aguilera <pedro.donosoa@sansano.usm.cl>
2020-08-10 22:40:12 -0400

    Chopinhauer API for Heroku v1.2.5

--- src/main/java/proyecto/Controlador/APIControlador.java
+++ src/main/java/proyecto/Controlador/APIControlador.java
@@ -17,7 +17,7 @@ public class APIControlador {
 
   @GetMapping("/")
   public String inicio(){
-    return "Hola Bienvenid@ a la API de Compumundohipermegared v1.2.4";
+    return "Hola Bienvenid@ a la API de Compumundohipermegared v1.2.5";
   }
   @GetMapping("/pservice")
   public String pserviceInicio(){
